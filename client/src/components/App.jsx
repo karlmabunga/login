dbed1f673d78a6858ae794f390a18f9dc1404492
Karl Mabunga <karlmabunga@yahoo.com>
2021-03-29 22:40:05 -0700

    Added two input fields for an email and password. The sign up button which doesn't do anything yet

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,7 +6,11 @@ export default function Login () {
   return (
     <div className='App'>
       <h2 className='header'>Login Below</h2>
-
-    </div>
+      <form>
+        <input type='text' name='email' placeHolder='Email'></input>
+        <input type='password' name='password' placeHolder='Password' />
+      </form>
+      <button className='signup'>Sign Up</button>
+    </div >
   )
 }
